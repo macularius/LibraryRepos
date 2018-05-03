d8c61bcf51adeebc159c33715863561f673c6511
Brainiack5 <35738996+Brainiack5@users.noreply.github.com>
2018-05-03 16:33:52 +0400

    Initcimmit
    Initcimmit

---
+++ Library/WebUI/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Entities;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository repository;
+        public int pageSize = 2;
+
+        public BookController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        public ViewResult List(int page = 1)
+        {
+            BooksListViewModel model = new BooksListViewModel
+            {
+                Books = repository.Books.OrderBy(book => book.BookId).Skip((page - 1) * pageSize).Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = /*repository.Books.Count()*/3
+                }
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ Library/WebUI/Models/BooksListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Domain.Entities;
+
+namespace WebUI.Models
+{
+    public class BooksListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
